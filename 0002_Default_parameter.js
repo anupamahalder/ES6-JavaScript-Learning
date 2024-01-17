acd3a2507e1b3728c981670746bc0c8d61b3341f
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-17 23:17:37 +0530

    In ES6 we can use default parameter to a function

---
+++ 0002_Default_parameter.js
@@ -0,0 +1,10 @@
+//--------- Default parameter ---------- 
+function say(message="Hello programmer"){
+    console.log(message);
+}
+
+
+
+// if do not pass any argument inside function then there will not show undefined 
+say();
+say("Hello all");
